218c6684fb933971ce1fd8cb395aca4ebf06944c
Eduardo Lucca <edlucca@student.hive.fi>
2025-09-24 17:05:55 +0300

    updated

--- src/dinner.c
+++ src/dinner.c
@@ -1,16 +1,6 @@
 
 #include "philo.h"
 
-bool	check_simulation(t_data *data)
-{
-	int philo_is_death;
-
-	pthread_mutex_lock(&data->dinner_over);
-	philo_is_death = data->stop_simulation;
-	pthread_mutex_unlock(&data->dinner_over);
-	return (true);
-}
-
 void	*routine(void *arg)
 {
 	t_philo *philo;
